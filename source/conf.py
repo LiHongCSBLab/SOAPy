80c3e178fe5233bfcb906fd407218688f67efba2
heqi <66099185+wheat-whq@users.noreply.github.com>
2025-01-14 22:54:14 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Heqi Wang'
 author = 'Cancer system biology lab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = '0.1.9'
 
 
 # -- General configuration ---------------------------------------------------
