df6aa42eebc675f5f69592547efc22cac37706a9
heqi <66099185+wheat-whq@users.noreply.github.com>
2025-02-25 13:19:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name='SOAPy-st',
-    version='1.0.0',
+    version='1.0.1',
     author='Cancer system biology lab',
     author_email='wangheqi2021@sinh.ac.cn',
     description='Spatial Omics Analysis in Python',
